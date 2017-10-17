1346806e88d49d58666b7da74c0039cf73423552
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-10-17 16:54:37 +0100

    UPDATE: routing among UI pages, removed visit proposalui

--- frontend/src/visitingscientist/visitingscientist.js
+++ frontend/src/visitingscientist/visitingscientist.js
@@ -22,6 +22,11 @@ export class Visitingscientist {
         "display": "PID: 1586 - API Test with Crystallisation and MX beamtime for acetylcholine"
       }
     ];
+    this.items=[{date:"06/09/2017",summary:"spectrum of strychnine process with v_noesy_pro.mac (NUTS-Pro) or v_noesy.mac (NUTS-2D)", info:"1.6 Mb"},
+      {date:"07/09/2017",summary:" spectrum of sucrose (1.3 Mbytes); process with v_ghsqc_pro.mac (NUTS-Pro) or v_ghsqc.mac (NUTS-2D)", info:"1.3 Mb"},
+      {date:"08/09/2017",summary:"spectrum of strychnine (2.1 Mbytes); process with v_hsqc_pro.mac (NUTS-Pro) or v_hsqc.mac (NUTS-2D).", info:"2.1 Mb"},
+    ]
+
   }
   attached() {
     console.log("VisitingScientist atached()")
