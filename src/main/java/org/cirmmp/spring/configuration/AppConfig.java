7fb87b61eaec68ed7996b367831c7a89f0a15b4b
Andrea <andreagia71@gmail.com>
2017-11-08 16:04:47 +0100

    Added OpenID security configuration

--- src/main/java/org/cirmmp/spring/configuration/AppConfig.java
+++ src/main/java/org/cirmmp/spring/configuration/AppConfig.java
@@ -42,6 +42,7 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+		registry.addResourceHandler("/frontEnd/**").addResourceLocations("/frontEnd/");
     }
     
     /**
