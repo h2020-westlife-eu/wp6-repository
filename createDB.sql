586258b48ce7a1641a81a315662d46c381968ac8
Andrea <andreagia71@gmail.com>
2017-11-08 17:19:45 +0100

    Added jetty configuration

--- createDB.sql
+++ createDB.sql
@@ -39,14 +39,15 @@ VALUES ('DBA');
 
 
 /* Populate one Admin User which will further create other users for the application using GUI */
+/* password: user   */
 INSERT INTO APP_USER(sso_id, password, first_name, last_name, email)
-VALUES ('andrea','$2a$10$4eqIF5s/ewJwHK1p8lqlFOEm2QIA0S8g6./Lok.pQxqcxaBZYChRm', 'Andrea','Giachetti','andrea@xyz.com');
+VALUES ('user','$2a$10$q1M2rwLvNFOXiArJAG7OFei49Aj1WJrF6CDveoAOEixUAk5e5uNWW', 'User','User','user@xyz.com');
 
 
 /* Populate JOIN Table */
 INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
   SELECT user.id, profile.id FROM app_user user, user_profile profile
-  where user.sso_id='andrea' and profile.type='ADMIN';
+  where user.sso_id='user' and profile.type='ADMIN';
 
 /* Create persistent_logins Table used to store rememberme related stuff*/
 CREATE TABLE persistent_logins (
