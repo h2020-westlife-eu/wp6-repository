92d28307230b9d5edc83950b0ee982c13ed5d217
TomasKulhanek <tomatonek12345>
2017-11-14 13:59:54 +0000

    UPDATE: screens

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,8 +3,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'West-Life Repository Router';
     config.map([
-      {route: ['', 'home'], name: 'home', moduleId: 'visitingscientist/visitingscientist', nav: true, title: 'Visiting Scientist'},
-      {route: 'repositorystaff', name: 'repositorystaff', moduleId: 'repositorystaff/repositorystaff', nav: true, title: 'Support Staff'},
+      {route: ['', 'home'], name: 'home', moduleId: 'visitingscientist/visitingscientist', nav: true, title: 'Dataset Dashboard'},
+      {route: 'repositorystaff', name: 'repositorystaff', moduleId: 'repositorystaff/repositorystaff', nav: true, title: 'File Upload Tool'},
     ]);
     this.router = router;
   }
