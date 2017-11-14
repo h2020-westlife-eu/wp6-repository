09c07c9785f5dcd68643c02547c904f9b13307cb
Tomas Kulhanek <tomaton@centrum.cz>
2017-11-14 13:49:33 +0000

    Update repositorystaff.js

--- frontend/src/repositorystaff/repositorystaff.js
+++ frontend/src/repositorystaff/repositorystaff.js
@@ -9,6 +9,9 @@ export class Repositorystaff {
       {date:"07/09/2017",summary:" spectrum of sucrose (1.3 Mbytes); process with v_ghsqc_pro.mac (NUTS-Pro) or v_ghsqc.mac (NUTS-2D)", info:"1.3 Mb"},
       {date:"08/09/2017",summary:"spectrum of strychnine (2.1 Mbytes); process with v_hsqc_pro.mac (NUTS-Pro) or v_hsqc.mac (NUTS-2D).", info:"2.1 Mb"},
     ]
+    this.filestoupload=[];
+    this.uploadfiles=[];
+    this.uploaddir="";
 
   }
   attached() {
@@ -22,4 +25,50 @@ export class Repositorystaff {
     let indexremoved = this.items.indexOf(item);
     if (indexremoved >=0 ) this.items.splice(indexremoved,1);
   }
+  
+    selectItemToUpload(item) {
+    console.log("selected item");
+    console.log(item);
+  }
+
+  removeItemToUpload(item){
+    console.log("selected item");
+    console.log(item);
+  let i = this.filestoupload.indexOf(item);
+    this.filestoupload.splice(i,1);
+  }
+
+  dropped(event){
+    console.log("Dropped")
+    console.log(event.dataTransfer.files);
+    event.stopPropagation();
+    event.preventDefault();
+    this.filestoupload.unshift(...event.dataTransfer.files);
+    return true;
+  }
+
+  dragged(event){
+    return true;//event.preventDefault(); event.dataTransfer.dropEffect = 'copy';
+  }
+
+  appendFiles(event){
+    console.log("appendFiles()");
+    console.log(event.target.files);
+    //this.uploadfiles=event.target.files;
+    this.filestoupload.unshift(...event.target.files);
+    //else this.filestoupload.unshift(event.target.files);
+  }
+
+  appendDir(event) {
+    console.log("appendDir")
+    console.log(event.target.files);
+    this.filestoupload.unshift(...event.target.files)
+  }
+
+  submitUpload(){
+    //TODO fake implementation
+    this.items.unshift(...this.filestoupload);
+    this.filestoupload=[];
+  }
+
 }
