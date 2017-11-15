0b5ca6d3cba307fe29d7809751323812782cc1fa
TomasKulhanek <tomatonek12345>
2017-11-15 13:30:41 +0000

    UPDATE: added picker component and repositorytovf page

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,6 +5,7 @@ export class App {
     config.map([
       {route: ['', 'home'], name: 'home', moduleId: 'visitingscientist/visitingscientist', nav: true, title: 'Dataset Dashboard'},
       {route: 'repositorystaff', name: 'repositorystaff', moduleId: 'repositorystaff/repositorystaff', nav: true, title: 'File Upload Tool'},
+      {route: 'repositorytovf', name: 'repositorytovf', moduleId: 'repositorytovf/repositorytovf', nav: true, title: 'Upload to Virtual Folder'},
     ]);
     this.router = router;
   }
