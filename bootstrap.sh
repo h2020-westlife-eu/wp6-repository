fe02823809069de28f4d39693b2f98d2fec02682
TomasKulhanek <tomatonek12345>
2017-11-22 13:04:01 +0000

    ADD: generating RANDOM db password

--- bootstrap.sh
+++ bootstrap.sh
@@ -111,8 +111,10 @@ fi
 yum install -y java mariadb-server maven
 service mariadb start
 
-#TODO generate random password, store it locally and distribute as a environment variable
-export DBCRED=and1217
+# generate random password, store it locally and distribute as a environment variable
+export DBCRED=`openssl rand -base64 32`
+echo DBCRED=${DBCRED} > /home/vagrant/.westlife/repository.key
+
 echo setting db
 mysql --user=root <<EOF
 use mysql
@@ -128,10 +130,7 @@ echo creating db
 # create db for backend
 echo populating data
 mysql --user=root --password=${DBCRED} < $WP6REPSRC/backend/createDB.sql
-#TODO link frontend directory to webapp doesn't work on shared folders, needs to be change in source code
-#ln -s $WP6REPSRC/frontend $WP6REPSRC/backend/src/main/webapp/frontend
-# change appropriate conf of repository service
+#backend app gets frontend location from environment variable REP_LOCATION
 sed -i -e "s/^\(WorkingDirectory\s*=\s*\).*$/\1\${WP6SRCESC}/backend/g" /etc/systemd/system/westlife-repository.service
 sed -i -e "s/^\(Environment=REP_LOCATION\s*=\s*\).*$/\1\${WP6SRCESC}/g" /etc/systemd/system/westlife-repository.service
 service westlife-repository start
-
