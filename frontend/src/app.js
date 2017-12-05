05536b2f477fee3437225a6415a735611d6b9534
TomasKulhanek <tomatonek12345>
2017-12-05 11:36:47 +0000

    ADD: check for mountpoint as directory, unmount if necesarry, fix issue #61

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,16 +2,10 @@ export class App {
 
   configureRouter(config, router) {
     config.title = 'West-Life Repository Router';
-    config.map([
-      {route: ['', 'home'], name: 'home', moduleId: 'scientist/visitingscientist', nav: true, title: 'Dataset Dashboard'},
-      {route: 'repositorystaff', name: 'repositorystaff', moduleId: 'repositorystaff/repositorystaff', nav: true, title: 'File Upload Tool'},
-      {route: 'repositorytovf', name: 'repositorytovf', moduleId: 'scientist/repositorytovf', nav: true, title: 'Upload to Virtual Folder'},
-    ]);
-    this.router = router;
   }
 
   login() {
-    window.location="login.html?next"+window.location.pathname;
+    //window.location="login.html?next"+window.location.pathname;
   }
 
 }
--- frontend/test/unit/app.spec.js
+++ frontend/test/unit/app.spec.js
@@ -1,4 +1,4 @@
-import {App} from '../../src/app';
+import {App} from '../../src/repository/app';
 
 describe('the app', () => {
   it('says hello', () => {
