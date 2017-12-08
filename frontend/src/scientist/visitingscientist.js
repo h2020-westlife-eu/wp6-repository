69ad2025783e670e11ad21091820e8c6f33f9edb
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-12-08 18:07:27 +0000

    UPDATE: project dataset selection

--- frontend/src/scientist/visitingscientist.js
+++ frontend/src/scientist/visitingscientist.js
@@ -36,12 +36,13 @@ export class Visitingscientist {
       }
     ];*/
     //demo data until backend is implemented
-    this.items=[{projectid:"1",date:"06/09/2017",summary:"spectrum of strychnine process with v_noesy_pro.mac (NUTS-Pro) or v_noesy.mac (NUTS-2D)", info:"1.6 Mb"},
-      {projectid:"1",date:"07/09/2017",summary:" spectrum of sucrose (1.3 Mbytes); process with v_ghsqc_pro.mac (NUTS-Pro) or v_ghsqc.mac (NUTS-2D)", info:"1.3 Mb"},
-      {projectid:"2",date:"08/09/2017",summary:"spectrum of strychnine (2.1 Mbytes); process with v_hsqc_pro.mac (NUTS-Pro) or v_hsqc.mac (NUTS-2D).", info:"2.1 Mb"},
+    this.itemsall=[{projectId:"1",date:"06/09/2017",summary:"spectrum of strychnine process with v_noesy_pro.mac (NUTS-Pro) or v_noesy.mac (NUTS-2D)", info:"1.6 Mb"},
+      {projectId:"1",date:"07/09/2017",summary:" spectrum of sucrose (1.3 Mbytes); process with v_ghsqc_pro.mac (NUTS-Pro) or v_ghsqc.mac (NUTS-2D)", info:"1.3 Mb"},
+      {projectId:"2",date:"08/09/2017",summary:"spectrum of strychnine (2.1 Mbytes); process with v_hsqc_pro.mac (NUTS-Pro) or v_hsqc.mac (NUTS-2D).", info:"2.1 Mb"},
     ]
+    this.items = this.itemsall.slice()
     this.showProposals=true;
-
+    this.showDatasets=true;
   }
 
   attached() {
@@ -65,19 +66,19 @@ export class Visitingscientist {
 
 
       });
-    /* not yet implemented on backend
+    //not yet implemented on backend
     this.httpclient.fetch("data")
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        this.items = data
+          this.itemsall = data
         })
       .catch(error => {
         //console.log(error);
         console.log(error);
-        alert("Sorry, error:"+error.statusCode+" "+error.message);
+        //alert("Sorry, error:"+error.statusCode+" "+error.message);
       });
-      */
+
   }
 
   switchMoreLessProposals() {
@@ -90,11 +91,26 @@ export class Visitingscientist {
     }
   }
   selectProposal(item) {
-    console.log(item);
+    console.log("SelectedProposal()")
+    console.log(item.projectId);
+
     this.selectedProposal=item;
     this.showProposals=false;
+    //now remember all datasets
+    //this.allitems = this.items.splice();
+    //filter per the selected project
+    this.items = this.itemsall.filter(filtereditem => filtereditem.projectId == item.projectId)
   }
   showAllProposals(){
     this.showProposals=true;
+    this.showDatasets=true;
+    this.items = this.itemsall.slice()
+  }
+
+  selectDataset(item) {
+    console.log("SelectedDataset()")
+    console.log(item);
+    this.showDatasets=false;
+    this.selectedDataset=item;
   }
 }
