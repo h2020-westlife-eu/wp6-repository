b7ac86c411dee9ae27d888b048c053dd42724da2
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-01-09 13:40:06 +0000

    UPDATE: path to frontend for demo proxies, admin contextpath,

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,6 +26,7 @@ cp -R $WP6REPSRC/conf-template/* /
 # replace path to frontend in configuration to current path
 export WP6SRCESC=$(echo $WP6REPSRC | sed 's_/_\\/_g')
 sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/repository\/latest\/frontend/${WP6SRCESC}\/frontend/g" /etc/httpd/conf.d/wp6-repository.conf
+for file in $(ls /etc/httpd/conf.d/proxy* ); do sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/repository\/latest/${WP6SRCESC}/g" "${file}";done
 #add +x permission on all html files which has include directive
 # some html pages includes header with menu, footer etc shared among pages
 #no html include needed anymore
