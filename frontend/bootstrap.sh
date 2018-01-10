3b1161d43981a695ef3ea7bd23ed4a2ab69a2255
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-01-10 17:39:50 +0000

    UPDATE: select project on url

--- frontend/bootstrap.sh
+++ frontend/bootstrap.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # bootstraps frontend compilation tools
 yum -y install npm
+npm install
 #on mounted FS - e.g. shared /vagrant folder 'npm install' cause errors
-npm install --no-bin-links
+#npm install --no-bin-links
 #if error appears - maximum call stack size exceeded, repeat
-npm install --no-bin-links
+#npm install --no-bin-links
 
 #on standard FS - issue
 #npm install
-npm install aurelia-cli -g
+sudo npm install aurelia-cli -g
 
 
 #au run --watch
