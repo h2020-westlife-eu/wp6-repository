549cf8a4729f5e6f83d7ff6ad410f35100c926c4
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-01-15 15:13:03 +0000

    ADD: update script for servers

--- update.sh
+++ update.sh
@@ -1,4 +1,19 @@
+#!/bin/sh
+echo asdf
 cd /home/vagrant/wp6-repository
+UPSTREAM=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+git fetch
+LOCAL=$(git rev-parse HEAD)
+echo $LOCAL
+REMOTE=$(git rev-parse origin/dev-tomas)
+echo $REMOTE
+BASE=$(git merge-base HEAD "$UPSTREAM")
+echo $BASE
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
 git pull
 sudo bash <<EOF
 service westlife-repository stop
@@ -6,3 +21,8 @@ service httpd stop
 export WP6SRC=`pwd`
 ./bootstrap.sh
 EOF
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
