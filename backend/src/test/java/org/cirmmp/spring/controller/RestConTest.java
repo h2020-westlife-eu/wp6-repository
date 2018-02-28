05f057a8967249bb112bdbea36f570012bf6f79e
Morris <chris.morris@stfc.ac.uk>
2018-02-28 07:59:59 +0000

    more tests for RestCon. Still to do: createSSOUser and files

--- backend/src/test/java/org/cirmmp/spring/controller/RestConTest.java
+++ backend/src/test/java/org/cirmmp/spring/controller/RestConTest.java
@@ -3,8 +3,13 @@ package org.cirmmp.spring.controller;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.cirmmp.spring.configuration.AppConfig;
+import org.cirmmp.spring.model.json.JProject;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,6 +18,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -50,6 +57,7 @@ public class RestConTest {
 	}
 
 	private RestCon restCon;
+	private org.springframework.security.core.userdetails.User  user;
 
 	@Before
 	public void setUp() throws Exception {
@@ -57,7 +65,7 @@ public class RestConTest {
 				RestCon.class
 		);
 
-		org.springframework.security.core.userdetails.User user = 
+		this.user = 
 			new org.springframework.security.core.userdetails.User(USERNAME, "password", 
 				true, true, true, true, 
 				Collections.emptySet()
@@ -87,32 +95,82 @@ public class RestConTest {
 
 	@Test
 	public void testListUsers() {
-		this.restCon.listUsers();
+		ResponseEntity<?> response = this.restCon.listUsers();
+		assertEquals(HttpStatus.OK, response.getStatusCode() );
 	}
-
+	
+	// example "{\"id\":629,\"userId\":59,\"creation_date\":\"Feb 27, 2018 5:23:37 PM\"}	";
+	static final Pattern ID  = Pattern.compile( "\"id\"\\:(\\d+)," );
+	
+	@Test
+	public void rt() {
+		assertTrue( Pattern.compile("a").matcher("aa").find() );
+	}
+	
 	@Test
 	public void testCreateProject() {
-		fail("Not yet implemented");
+		
+		// create a project
+		JProject jProject = new JProject();
+		ResponseEntity<?> response = this.restCon.createProject(
+				jProject ,
+				this.user.getUsername(), "", "", ""
+		);
+		assertEquals(HttpStatus.OK, response.getStatusCode() );
+		
+		Matcher m = ID.matcher((String)response.getBody());
+		assertTrue((String)response.getBody(), m.find());
+		String id = m.group(1);
+		
+		// check it is in the list of projects
+		response = this.restCon.listProject(
+				this.user.getUsername(), "", "", ""
+		);
+		assertEquals(HttpStatus.OK, response.getStatusCode() );
+		// check new project is in list		
+		String json = (String) response.getBody();
+		assertTrue( ((String)response.getBody()).contains(id) );
+		
+		// list its files
+		response = this.restCon.listDataset(Optional.of( Long.parseLong(id) ));
+		List datasets = (List) response.getBody();
+		assertEquals(0, datasets.size());
+		
+		//TODO delete the project again
 	}
 
-	@Test
-	public void testListProject() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testListFiles() {
-		fail("Not yet implemented");
+		ResponseEntity<?> response = this.restCon.listFiles();
+		assertEquals(HttpStatus.OK, response.getStatusCode() );
+		// TODO check response
 	}
 
 	@Test
 	public void testListDataset() {
-		fail("Not yet implemented");
+		
+		// all projects
+		Optional<Long> nonesuch = Optional.empty();
+		ResponseEntity<?> response = this.restCon.listDataset(nonesuch );
+		assertEquals(HttpStatus.OK, response.getStatusCode() );
+		List datasets = (List) response.getBody();
+		// TODO check response
+		
+		// non-existent project 
+		response = this.restCon.listDataset(Optional.of(-1L));
+		// TODO shouldn't that be 404?
+		datasets = (List) response.getBody();
+		assertEquals(0, datasets.size());
 	}
 
 
 	@Test
 	public void testListOrder() {
-		fail("Not yet implemented");
+		ResponseEntity<?> response = this.restCon.listOrder(
+				this.user.getUsername(), "", "", ""
+		);
+		assertEquals(HttpStatus.OK, response.getStatusCode() );
+		// TODO check response
 	}
 }
